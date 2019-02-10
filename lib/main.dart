eadbc95ae583eb40e1a90cf6b3baec239fb60500
teddyphotos <goelkashish4@gmail.com>
2019-02-10 18:36:44 +0530

    Changed App Bar Colour

--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColor: Colors.green,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
