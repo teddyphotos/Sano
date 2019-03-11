08474c899b8c1bcfedb38a61fb309b6cefd62917
teddyphotos <goelkashish4@gmail.com>
2019-03-11 11:24:16 +0530

    Fixed Home.dart returning null error

--- lib/home.dart
+++ lib/home.dart
@@ -10,39 +10,36 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     int _selectedIndex = 1;
-    @override
-    Widget build(BuildContext context) {
-      final List<Widget> _children = [mapScreen(), mapScreen(), mapScreen()];
-
-      void _onTabTapped(int index) {
-        setState(() {
-          _selectedIndex = index;
-        });
-      }
-
-      return MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            title: Text("Sano: Emergency Help"),
-            backgroundColor: Colors.redAccent,
-          ),
-          body: _children[_selectedIndex],
-          bottomNavigationBar: BottomNavigationBar(
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.edit_location), title: Text("Location")),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.favorite), title: Text("Get Ambulance")),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.account_circle), title: Text("Options")),
-            ],
-            fixedColor: Colors.redAccent,
-            currentIndex: _selectedIndex,
-            onTap: _onTabTapped,
-          ),
-        ),
-      );
+    final List<Widget> _children = [mapScreen(), mapScreen(), mapScreen()];
+    void _onTabTapped(int index){
+      setState(() {
+        _selectedIndex = index;
+      });
     }
+    return
+
+      Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Sano: Emergency Help"),
+        backgroundColor: Colors.redAccent,
+      ),
+
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.edit_location), title: Text("Location")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite), title: Text("Get Ambulance")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle), title: Text("Options")),
+          ],
+        fixedColor: Colors.redAccent,
+        currentIndex: _selectedIndex,
+        onTap: _onTabTapped,
+      ),
+    );
   }
 }
+
