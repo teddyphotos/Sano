53ede186d078e0814db42804cb08ee563e3b553f
teddyphotos <goelkashish4@gmail.com>
2019-05-11 13:16:39 +0530

    Fixed app crash

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -4,13 +4,11 @@
 
 #import "GeneratedPluginRegistrant.h"
 #import <google_maps_flutter/GoogleMapsPlugin.h>
-#import <location/LocationPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [FLTGoogleMapsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleMapsPlugin"]];
-  [LocationPlugin registerWithRegistrar:[registry registrarForPlugin:@"LocationPlugin"]];
 }
 
 @end
---
+++ lib/login.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class login extends StatefulWidget {
+  @override
+  _loginState createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
