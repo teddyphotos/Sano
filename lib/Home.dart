66883919921d015c46cf497625f0d943a378d7fb
kasperonline <tharejatanmay@gmail.com>
2019-08-20 12:22:14 +0530

    Deleted options.dart and added Drawer on clicking options

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'dart:async';
-import 'options.dart';
 import 'legalStuff.dart';
 import 'mapScreen.dart';
 
@@ -14,6 +13,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   PermissionStatus _status;
   int _selectedIndex = 1;
+  final GlobalKey<ScaffoldState>_scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
@@ -25,18 +25,29 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    final List<Widget> _children = [legalStuff(), mapScreen(), options()];
+    final List<Widget> _children = [legalStuff(), mapScreen(), mapScreen()];
     void _onTabTapped(int index) {
       setState(() {
         _selectedIndex = index;
+        if(index==2)
+        {
+          _scaffoldKey.currentState.openDrawer();
+          index=1;
+          _onTabTapped(index);
+        }
       });
     }
 
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Sano: Emergency Help"),
-        backgroundColor: Colors.redAccent,
+        leading: Container(),
+      ),
+      drawer: Drawer(
+        child: DrawerHeader(
+          child: Text("Yo"),
+        ),
       ),
       body: _children[_selectedIndex],
       floatingActionButton: FloatingActionButton.extended(
--- lib/options.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-class options extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
